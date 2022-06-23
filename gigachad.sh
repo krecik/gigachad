4a30ed3157d7e6f0762ec9fcfc95c23ebaff10ce
Tomasz Skręt <tomasz.skret@spdg.pl>
2022-06-23 23:22:42 +0200

    my contribution on day 2022-06-23

--- gigachad.sh
+++ gigachad.sh
@@ -6,9 +6,7 @@ start_date=2022-01-01
 end=2023-01-01
 while [ "$start" != $end ]; do
   echo $start
-  $RANGE=365
-  number=$RANDOM
-  let "number %= $RANGE"
+  number=$(( ( RANDOM % 365 )  + 1 ))
   start=$(date -I -d "$start_date + $number day")
   touch $start$RANDOM
   git add .
